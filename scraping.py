2d542919b1d40aa7a7d84a265aebe6e8f17aaab7
Jesse Hernandez <jessehernandez@Jesses-Home-MBPro.local>
2022-06-18 22:20:22 -0700

    Module 10 Challenge Deliverable 3

--- scraping.py
+++ scraping.py
@@ -106,7 +106,7 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html(classes=["table", "table-bordered", "table-striped"])
 
 def hemisphere_images(browser):
     # 1. Use browser to visit the URL
